d364b1534eab68f2b5dca12f70365028a8f1cc31
kartik16052002 <choudharykartik1605@gmail.com>
2024-08-03 01:58:29 +0530

    changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbgr = require('debug')("development:mongoose");
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/scatch";
+const mongoUri = process.env.MONGODB_URI;
 
 mongoose
 .connect(mongoUri)
